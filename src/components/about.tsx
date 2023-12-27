47934a1db2f878ed2640dee05e770cc19332531e
Maycon Vinicius <contato@mayconviniciusdev.com>
2023-12-27 19:31:33 -0300

    Updating illustrative image, grid and CTA button colors

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -40,17 +40,17 @@ export const About = () => {
         
         <Image className="my-10 mx-auto"
         src="/img03.jpg"
-        width={350}
-        height={350}
+        width={300}
+        height={300}
         alt="Imagem ilustrativa"/>
 
         <div className="grid grid-cols-3 gap-4 text-white text-lg">
-          <div className="flex flex-col items-center justify-center bg-golden-wellow p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/80 p-6 rounded-2xl">
             <FaFeather className="mb-2 text-2xl"/>
             <p>Design intuitivo e amigável</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center col-span-2 bg-golden-wellow p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center col-span-2 bg-wellows/95 p-6 rounded-2xl">
             <div className="flex gap-4">
               <FaEarthAmericas className="mb-2 text-2xl"/>
               <FaDesktop className="mb-2 text-2xl"/>
@@ -58,32 +58,32 @@ export const About = () => {
             <p>Acesse de qualquer lugar e também de qualquer dispositivo</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-golden-wellow p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/95 p-6 rounded-2xl">
             <FaShapes className="mb-2 text-2xl"/>
             <p>100% personalizável e editável</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-golden-wellow p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/80 p-6 rounded-2xl">
             <FaRoute className="mb-2 text-2xl"/>
             <p>Foque em metas e produtividade</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-golden-wellow p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/95 p-6 rounded-2xl">
             <FaCircleHalfStroke className="mb-2 text-2xl"/>
             <p>Modo noturno</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center col-span-2 bg-golden-wellow p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center col-span-2 bg-wellows/95 p-6 rounded-2xl">
             <FaChartColumn className="mb-2 text-2xl"/>
             <p>Acompanhe teus resultados de forma simples, tangível e palpável</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-golden-wellow p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/80 p-6 rounded-2xl">
             <FaUserGear className="mb-2 text-2xl"/>
             <p>Suporte técnico e dedicado</p>
           </div>
         </div>
-        <button className="bg-golden-wellow hover:bg-wellows/80 duration-500 shadow-wellows/90 shadow-lg mt-10 px-28 h-12 rounded-xl text-white text-lg font-bold">QUERO ACESSAR AGORA!</button>
+        <button className="bg-wellows hover:bg-wellows/80 duration-500 shadow-wellows/70 shadow-lg mt-10 px-28 h-12 rounded-xl text-white text-lg font-bold">QUERO ACESSAR AGORA!</button>
       </section>
     </>
   )
