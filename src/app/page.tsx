189b04db3345e5b6460b5487eab03a02c4f4e453
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-01-04 12:04:50 -0300

    Adding footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/header";
 import { Testimonial } from "@/components/testimonial";
 import { Promise } from "@/components/promise";
 import { Faq } from "@/components/faq";
+import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Testimonial/>
       <Promise/>
       <Faq/>
+      <Footer/>
     </main>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <header className="bg-dark">
+      <div className="py-5 text-center max-w-2xl mx-auto text-white">
+        <h2 className="font-bold text-lg">TODOS OS DIREITOS RESERVADOS.</h2>
+        <p>2024</p>
+      </div>
+    </header>
+  )
+}
