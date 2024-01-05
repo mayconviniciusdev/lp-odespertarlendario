38ca619cac6c33ab5551d52283705d2abcb3b6f6
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-01-04 22:22:12 -0300

    Updating about codes and layout

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,88 +2,91 @@ import Image from "next/image"
 import { FaChartColumn, FaCircleHalfStroke, FaDesktop, FaEarthAmericas, FaFeather, FaRoute, FaShapes, FaSquareCheck, FaUserGear } from "react-icons/fa6";
 
 export const About = () => {
+  const benefits = [
+    {benefits: 'Clareza de objetivos'},
+    {benefits: 'Redução da procrastinação'},
+    {benefits: 'Criação de hábitos saudáveis'},
+    {benefits: 'Crescimento pessoal contínuo'},
+    {benefits: 'Metas e objetivos realizáveis'},
+    {benefits: 'Aumento da produtividade'}, ] 
+
   return (
     <>
-      <section className="py-20 flex items-center justify-center gap-20">
-        <div className="max-w-md">
+      <section className="container mx-auto py-12 px-5 text-center lg:flex items-center justify-center gap-20">
+        <div className="max-w-md mx-auto mb-10 lg:m-0">
           <h2 className="font-bold text-xl"><strong className="text-golden-wellow underline">Quer tomar as rédeas da sua vida</strong> saindo de ciclos ruins e improdutivos?</h2>
           <p className="py-5">O <strong>Despertar lendário</strong> ajudará você em sua transformação de vida, conquistando metas e sonhos de uma forma leve e visual.</p>
           <p>Esse <strong>sistema</strong> foi desenvolvido para que você foque naquilo que verdadeiramente importa, tendo <strong>uma visão palpável</strong> e geral do que necessita ser executado em prol <strong>dos seus objetivos</strong>.</p>
         </div>
-
-        <Image
+        <Image className="mx-auto lg:m-0"
         src="/img02.jpg"
         width={500}
         height={500}
         alt="Imagem ilustrativa"/>
       </section>
 
-      <section className="bg-dark">
-        <div className="py-14 text-center max-w-2xl mx-auto text-white">
-          <h2 className="text-xl font-bold">O que o <strong className="text-wellows underline">Despertar Lendário</strong> irá te fornecer?</h2>
-          <div className="my-8 max-w-xl mx-auto shadow-2xl shadow-wellows rounded-2xl">
-            <ul className="bg-white text-dark text-lg py-6 rounded-2xl">
-              <li><FaSquareCheck className="text-wellows mr-1 inline-block"/>Redução da procrastinação</li>
-              <li><FaSquareCheck className="text-wellows mr-1 inline-block"/>Crescimento pessoal contínuo</li>
-              <li><FaSquareCheck className="text-wellows mr-1 inline-block"/>Desenvolvimento de hábitos saudáveis</li>
-              <li><FaSquareCheck className="text-wellows mr-1 inline-block"/>Metas e objetivos realizáveis</li>
-              <li><FaSquareCheck className="text-wellows mr-1 inline-block"/>Aumento da produtividade</li>
-              <li><FaSquareCheck className="text-wellows mr-1 inline-block"/>Clareza de objetivos</li>
+      <section className="bg-dark px-5">
+        <div className="py-12 text-center container mx-auto text-white">
+          <h2 className="text-xl max-w-md mx-auto font-bold">O que você irá encontrar e obter com o template <strong className="text-wellows underline">Despertar Lendário?</strong></h2>
+          <div className="my-12 max-w-xl mx-auto shadow-2xl shadow-wellows rounded-2xl">
+            <ul className="bg-white text-dark/70 font-semibold py-6 rounded-2xl">
+              {benefits.map((item)=> (<li className="flex items-center justify-center my-1"><FaSquareCheck className="text-wellows mr-1 text-xl"/>{item.benefits}</li>))}
             </ul>
           </div>
-          <button className="bg-wellows hover:bg-wellows/80 duration-500 mt-10 px-20 h-12 rounded-xl text-white text-lg font-bold">QUERO ACESSAR AGORA!</button>
+          <button className="bg-wellows hover:bg-wellows/80 duration-500 mt-8 h-12 rounded-xl px-4 text-lg font-bold md:px-24">QUERO ACESSAR AGORA!</button>
         </div>
       </section>
 
-      <section className="py-14 text-center max-w-2xl mx-auto">
+      <section className="container mx-auto py-12 px-5 text-center">
+        <div className="max-w-2xl mx-auto">
         <h2 className="text-xl font-bold ">Um <strong className="text-golden-wellow underline">sistema para organização de vida</strong> em tempo recorde em um único lugar, foque seu tempo  no que realmente importa!</h2>
-        
-        <Image className="my-10 mx-auto"
+        <Image className="my-10 mx-auto px-4"
         src="/img03.png"
         width={400}
         height={400}
         alt="Imagem ilustrativa"/>
 
-        <div className="grid grid-cols-3 gap-4 text-white text-lg">
-          <div className="flex flex-col items-center justify-center bg-wellows/80 p-6 rounded-2xl">
+        <div className="text-dark/70 text-lg font-semibold lg:grid grid-cols-3">
+          <div className="flex flex-col items-center justify-center bg-wellows/70 p-6 m-2 rounded-2xl">
             <FaFeather className="mb-2 text-2xl"/>
             <p>Design intuitivo e amigável</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center col-span-2 bg-wellows/95 p-6 rounded-2xl">
-            <div className="flex gap-4">
-              <FaEarthAmericas className="mb-2 text-2xl"/>
-              <FaDesktop className="mb-2 text-2xl"/>
+          <div className="flex flex-col items-center justify-center col-span-2 bg-wellows/70 p-6 m-2 rounded-2xl">
+            <div className="flex gap-4 mb-2 text-2xl">
+              <FaEarthAmericas/>
+              <FaDesktop/>
             </div>
             <p>Acesse de qualquer lugar e também de qualquer dispositivo</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-wellows/95 p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/70 p-6 m-2 rounded-2xl">
             <FaShapes className="mb-2 text-2xl"/>
             <p>100% personalizável e editável</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-wellows/80 p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/70 p-6 m-2 rounded-2xl">
             <FaRoute className="mb-2 text-2xl"/>
             <p>Foque em metas e produtividade</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-wellows/95 p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/70 p-6 m-2 rounded-2xl">
             <FaCircleHalfStroke className="mb-2 text-2xl"/>
             <p>Modo noturno</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center col-span-2 bg-wellows/95 p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center col-span-2 bg-wellows/70 p-6 m-2 rounded-2xl">
             <FaChartColumn className="mb-2 text-2xl"/>
             <p>Acompanhe teus resultados de forma simples, tangível e palpável</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center bg-wellows/80 p-6 rounded-2xl">
+          <div className="flex flex-col items-center justify-center bg-wellows/70 p-6 m-2 rounded-2xl">
             <FaUserGear className="mb-2 text-2xl"/>
             <p>Suporte técnico e dedicado</p>
           </div>
         </div>
-        <button className="bg-wellows hover:bg-wellows/80 duration-500 shadow-wellows/70 shadow-lg mt-10 px-36 h-12 rounded-xl text-white text-lg font-bold">QUERO ACESSAR AGORA!</button>
+        <button className="bg-wellows hover:bg-wellows/80 duration-500 shadow-wellows/70 shadow-lg mt-5 px-4 h-12 rounded-xl text-white text-lg font-bold md:px-36">QUERO ACESSAR AGORA!</button>
+        </div>
       </section>
     </>
   )
