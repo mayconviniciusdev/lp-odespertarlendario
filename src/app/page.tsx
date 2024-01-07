c0ef55881cb79159e41ac75057f52c895f3cbeed
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-01-07 20:05:14 -0300

    Updating the testmonial component import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { About } from "@/components/about";
 import { Header } from "@/components/header";
-import { Testimonial } from "@/components/testimonial";
+import { Testimonial } from "@/components/testemonial/testimonial";
 import { Promise } from "@/components/promise";
 import { Faq } from "@/components/faq";
 import { Footer } from "@/components/footer";
