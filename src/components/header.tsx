40516be41c00f2a5d899c54fb97a873d0cd24615
Maycon Vinicius <contato@mayconviniciusdev.com>
2024-01-07 19:58:07 -0300

    Updating header title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="bg-dark px-5">
       <div className="py-12 mx-auto container text-center text-white">
-        <h2 className="max-w-2xl mx-auto font-bold text-3xl">Seja o <strong className="text-golden-wellow underline">protagonista</strong> da sua própria vida e crie um estilo de vida <strong className="text-golden-wellow underline">ultra lendário</strong>.</h2>
+        <h2 className="max-w-lg mx-auto font-bold text-xl">Seja o <strong className="text-golden-wellow underline">protagonista</strong> da sua própria vida e crie um estilo de vida <strong className="text-golden-wellow underline">ultra lendário</strong>.</h2>
         <p className="my-8 max-w-xl mx-auto">Nosso template é mais que só uma simples ferramenta, ele é um catalisador onde você conseguirá substituir uma vida de estagnação, por uma de evolução gradual e contínua.</p>
         <Image className="mx-auto px-4"
         src="/img01.png"
